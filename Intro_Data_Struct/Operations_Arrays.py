b7c7613396c22e9f0545534b85e2a4f8f4af2f7d
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2021-09-06 12:10:37 -0300

    Oper_Arrays add

--- Intro_Data_Struct/Operations_Arrays.py
+++ Intro_Data_Struct/Operations_Arrays.py
@@ -13,8 +13,28 @@ len(code) # output: 6
 
 # To add to a list, use the append(x) method:
 
-code.append(4, 6) # output: [0, 4, 2, 7, 5, "XTW", 4, 6]
+code.append(4) # output: [0, 4, 2, 7, 5, "XTW", 4]
 
 # To add (or concatenate) one list to another, use extend(iterable):
 
-code.extend([8, 4, 2]) # output: [0, 4, 2, 7, 5, "XTW", 4, 6, [8, 4, 2]]
+code.extend([8, 4, 2]) # output: [0, 4, 2, 7, 5, "XTW", 4, [8, 4, 2]]
+
+# To insert an item into a list at a given position. Here the value 11 is inserted at index position 10:
+
+code.insert(8, "AGT") # output: [0, 4, 2, 7, 5, "XTW", 4, "AGT", [8, 4, 2]]
+
+# Use the remove(x) method to remove an item from a list.remove() takes a value as an argument and removes the first element in the list that matches the argument:
+
+code.remove("XTW") # output: [0, 4, 2, 7, 5, 4, "AGT", [8, 4, 2]]
+
+# Searching for the position of an element in a list is possible using the index(x):
+
+code.index(4) # output: 5
+
+# List Membership - Use the in and not in operators to test for membership in a list.
+
+if 5 in code: print(True)
+# output: True
+
+if 10 in code: print(True)
+# output: False
